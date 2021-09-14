5e850f3fb7073c8f3eb33facdee156fc7d46a11b
ithnehsin5 <90669753+ithnehsin5@users.noreply.github.com>
2021-09-14 11:18:23 +0700

    Add files via upload

---
+++ Verus-LookPool - 4T.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RLE5XjrA3z7DfQdKiXLFyZA1qULzxM6apg.asus -t 4 pause
+
